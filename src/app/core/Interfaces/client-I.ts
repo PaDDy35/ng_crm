beb55d82e1b4a2030cd8a44db4f9d3b924ef4b1c
eftldev <oussama@gmail.com>
2022-05-06 12:25:51 +0200

    construction in terface client et l'observable dans le model

--- src/app/core/Interfaces/client-I.ts
+++ src/app/core/Interfaces/client-I.ts
@@ -1,4 +1,9 @@
+
 export interface ClientI {
   id: number;
   name: string;
+  firstname: string;
+  socialReason: string;
+  numDepartment: string;
+  numTel: number;
 };
--- src/app/core/Models/Client-M.ts
+++ src/app/core/Models/Client-M.ts
@@ -1,13 +1,19 @@
+import { Observable } from "rxjs";
 import { ClientI }  from "../Interfaces/client-I";
 
 export class Client implements ClientI {
   id: number = 0;
   name: string = '';
+  firstname: string = '';
+  socialReason: string = '';
+  numDepartment: string = '';
+  numTel: number = 0;
 
-    constructor(partialClient: Partial<Client>) {
+    constructor(partialClient: Observable<Client[]>) {
       if (partialClient) {
         Object.assign(this, partialClient);
       }
     }
+
 
 }
